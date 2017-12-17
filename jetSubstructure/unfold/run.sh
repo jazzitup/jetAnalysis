dd64e536626493de1d8fab0dfc2051e53b8c99ea
Yongsun Kim <kimy@cern.ch>
2017-12-16 21:35:01 -0600

    backup

--- jetSubstructure/unfold/run.sh
+++ jetSubstructure/unfold/run.sh
@@ -1,5 +1,15 @@
 root -l <<EOF
 gSystem->Load("libRooUnfold");
 .L unfoldPtMass.cxx+
-unfoldPtMass()
 EOF
+
+while read line
+do
+    for optX in 1
+    do
+	for optY in 4 
+	do
+	    root -l -b -q 'unfoldPtMass.cxx+('$optX','$optY',"'$line'")'
+	done
+    done
+done < list.txt
